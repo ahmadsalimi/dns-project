772e90cb1203d035e4c10de67411d1ca87b4c452
ahmadsalimi <ahsa9978@gmail.com>
2023-06-26 16:20:37 +0330

    Add configuration model

--- src/messenger/models.py
+++ src/messenger/models.py
@@ -1,3 +1,36 @@
+from typing import Optional
+
 from django.db import models
 
-# Create your models here.
+
+class Configuration(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    value = models.CharField(max_length=1024)
+
+    @classmethod
+    def get(cls, name: str, default=None) -> Optional[str]:
+        try:
+            return cls.objects.get(name=name).value
+        except cls.DoesNotExist:
+            return default
+
+    @classmethod
+    def set(cls, name: str, value: str):
+        try:
+            config = cls.objects.get(name=name)
+            config.value = value
+            config.save()
+        except cls.DoesNotExist:
+            cls.objects.create(name=name, value=value)
+
+    @classmethod
+    def exists(cls, name: str) -> bool:
+        return cls.objects.filter(name=name).exists()
+
+    def __str__(self):
+        return self.name + ': ' + self.value
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+        ]
